5649e5b4429158b1ca4b08e332ce0b39bddbb89f
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-06-03 12:49:40 +0300

    dom

---
+++ app.js
@@ -0,0 +1,25 @@
+// DOM = document opjet model
+// 1. getElementsByTagName
+
+console.log(document.getElementsByTagName("h1"));
+
+// 2. getElementsById
+
+console.log(document.getElementById("main"));
+
+// 3. getElementByClassName
+
+console.log(document.getElementsByClassName("cls"));
+
+
+
+// 4. querySelector
+
+console.log(document.querySelector("h1"));
+
+console.log(document.querySelector("#main"));
+
+
+
+const h1 = document.querySelector("h1")
+console.log(h1)
